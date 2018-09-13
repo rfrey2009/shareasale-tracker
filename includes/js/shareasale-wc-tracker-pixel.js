375002ce877898177558df20d63a555ab3f22b90
Ryan Stark <rfrey2009@localhost.localdomain>
2018-09-13 16:40:43 -0500

    added a comment

--- includes/js/shareasale-wc-tracker-pixel.js
+++ includes/js/shareasale-wc-tracker-pixel.js
@@ -8,7 +8,7 @@ try {
 	shareasaleWcTrackerPixelImg.setAttribute('data-no-lazy', 1);
 	document.body.appendChild(shareasaleWcTrackerPixelImg);
 	shareasaleWcTrackerTriggered();
-}
+} //this is necessary because some optimization/caching scripts disrupt WP localized data found in <script> tags, so shareasaleWcTrackerPixelImg assignments above fail.
 catch (e) {
 	console.log('ShareASale JS failed because ' + e + ', so unwrapped <noscript> as a fallback');
 	var shareasaleWcTrackerPixelFallback = document.querySelector('#_SHRSL_noscript_1').textContent;
